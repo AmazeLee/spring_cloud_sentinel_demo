d1b8e5f835ff4de492988477fc1040ad7cbd5523
leeshuhua <leeshuhua@163.com>
2020-08-19 16:07:19 +0800

    sentinel

---
+++ order_service_rest/src/main/java/cn/itcast/order/exception/ExceptionUtils.java
@@ -0,0 +1,24 @@
+package cn.itcast.order.exception;
+
+
+import com.sun.deploy.net.HttpRequest;
+
+public class ExceptionUtils {
+
+	/**
+	 * 静态方法
+	 *      返回值: SentinelClientHttpResponse
+	 *      参数 : request , byte[] , clientRquestExcetion , blockException
+	 */
+	//限流熔断业务逻辑
+	public static SentinelClientHttpResponse handleBlock(HttpRequest request, byte[] body, ClientHttpRequestExecution execution, BlockException ex) {
+		return new SentinelClientHttpResponse("abc");
+	}
+
+	//异常降级业务逻辑
+	public static SentinelClientHttpResponse handleFallback(HttpRequest request, byte[] body,
+		ClientHttpRequestExecution execution, BlockException ex) {
+		return new SentinelClientHttpResponse("def");
+	}
+
+}
